9fd8c2d4ec30eb27585658acebc862ffc47ac411
Miguel Cachero <69327018+kuuueee@users.noreply.github.com>
2023-06-07 11:58:42 -0700

    Alex' Files

---
+++ good.js
@@ -0,0 +1,42 @@
+
+
+function right(message){
+    alert("right answser");
+alert(message);
+
+
+}
+function wrong(message){
+    alert("wrong answser");
+alert(message);
+
+}
+
+function bruh(message){
+    alert("bro really.....");
+alert(message);
+
+}
+
+function broooWhat(message){
+    alert("that does not seem right");
+alert(message);
+
+}
+
+function idk (message){
+    alert("that does not seem right");
+alert(message);
+
+}
+
+
+function hide_this(id){
+    document.getElementById(id).style.display = "none";
+    
+}
+
+function show_next(id){
+    document.getElementById(id).style.display = "block"; 
+}
+
